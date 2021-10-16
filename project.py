418b85f3a2d2d5de1b33d9d44132dd67a3d987f8
BrianCIS <85715367+BrianCIS@users.noreply.github.com>
2021-10-15 19:38:23 -0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,8 @@
+import mysql.connector
+from mysql.connector import Error
+from sql import create_connection
+from sql import execute_query
+from sql import execute_read_query
+import datetime
+from datetime import date
+
